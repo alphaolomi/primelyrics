4bbf7b3c10becafcca610a9a0b0e131f0a619479
Alpha Olomi <alphaolomi@gmail.com>
2021-11-15 07:49:17 +0300

    fix: lyrics page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Tracks from "@/components/Tracks/Tracks";
 import Search from "@/components/Search/Search";
+import AppNavbar from "@/components/AppNavbar";
 
 const Index: React.FC = () => {
     return (
         <main>
+            <AppNavbar />
             <div className="mt-3">
                 <Search />
             </div>
--- src/components/Lyrics/_Lyrics.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Lyrics = () => <div data-testid="Lyrics">Lyrics Component</div>;
-
-export default Lyrics;
